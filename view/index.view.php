87bbeb95a409e62cf13cfcdc8028716d459022dd
giedrius strazdas <kgs.kns@gmail.com>
2022-05-29 11:07:02 +0300

    html table nuskaitoma is failo,data iterpiami is masyvo

--- view/index.view.php
+++ view/index.view.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Flight Ticket</title>
 </head>
-<!--DDEV VISAI NEVEIKĖ STYLE.CSS O PHPSTORM - NENUSKAITINĖJO DUOMENŲ, DĖL TO ĮRAŠIAU INCLUDE ČIA, NES NEĮMANOMA DIRBTI - SUGAIŠAU TAM DIENĄ-->
+<!--DDEV NEVEIKĖ STYLE.CSS O PHPSTORM - NENUSKAITĖ DUOMENŲ, DĖL TO ĮRAŠIAU INCLUDE ČIA, DĖL TECH. PROBLEMŲ-->
 <?php include "../data/cities.php"; include "../data/luggage.php"; include "../data/flights.php"; include "../data/prices.php";?>
 
 <?php $date = new DateTime("now", new DateTimeZone('Europe/Vilnius'));?>
@@ -16,19 +16,23 @@
 
 <div class="container">
     <?php if(isset($_POST['save'])){
-        if ($_POST['luggageWeight'] > 20) {
-            $lugTax=20 and $_POST['price'] +=20;
-        } else {
-            $lugTax="";
+        if ($_POST['luggageWeight'] > 20) {$lugTax=20 and $_POST['price'] +=20;} else {$lugTax="";}
+        $seqArray = array_values($_POST); $seqArray[5] = $seqArray[7]; $seqArray[7] = $_POST['price']; $seqArray[6] = $lugTax;
+//        var_dump($_POST); var_dump($seqArray);
+        $lines = file('table.txt'); $i = 0;
+        foreach ($lines as $num=>$line)
+        {
+            echo $line;
+            if ($num == 0) {
+                echo $date->add(DateInterval::createFromDateString("2 days"))->format("Y-m-d H:i");}
+            elseif ($num == 3) {
+                echo $date->add(DateInterval::createFromDateString('16 days'))->format('Y-m-d H:i');}
+            elseif ($num == 6) {
+                echo $_POST['firstName'].' '.$_POST['lastName'];}
+            else {
+                echo $seqArray[$i]; ++$i;
+            }
         }
-        echo '<br><table class="ticket"><thead><tr><th colspan="20">Check your flight information before flight</th></tr></thead><tbody><tr><td colspan="3" rowspan="3">';
-echo $date->add(DateInterval::createFromDateString("2 days"))->format("Y-m-d H:i"); echo '</td><td colspan="2" class="one">From:</td><td colspan="8">';
-echo $_POST['sourceAirport']; echo '</td><td colspan="7" class="one two">Additional Information:</td></tr>';echo '<tr><td colspan="2" class="one">To:</td><td colspan="8">';
-echo $_POST['destinationAirport']; echo '</td><td colspan="3" class="one two">Return Flight:</td><td colspan="4" class="two">'; echo $date->add(DateInterval::createFromDateString('16 days'))->format('Y-m-d H:i');
-echo '</td></tr><tr><td colspan="3" class="one">Flight Number:</td><td colspan="7">'; echo $_POST['flightNr']; echo '</td><td colspan="5" class="one two">Price:</td><td colspan="2" class="two">';
-echo $_POST['price']; echo '</td></tr>'; echo '<tr><td colspan="4" class="one">Passenger Name:</td><td colspan="9">'; echo $_POST['firstName'].' '.$_POST['lastName']; echo '</td><td colspan="5" class="one two">Luggage Weight:</td><td colspan="2" class="two">';
-echo $_POST['luggageWeight']; echo '</td></tr><tr><td colspan="4" class="one">Passenger ID Number:</td><td colspan="9">'; echo $_POST['idNumber']; echo '</td><td colspan="5" class="one two">Luggage Tax:</td><td colspan="2" class="two">';
-echo $lugTax;echo '</td></tr><tr><td colspan="13"></td><td colspan="5" class="one two">Total Price:</td><td colspan="2" class="two">'; echo $_POST['price'] ; echo '</td></tr></tbody></table>';
   }
     ?>
 
